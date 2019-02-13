2a6f4b2389be79766a020c73109a90a03f8a7a51
Eduard Iablonschi <s188800@uos.ac.uk>
2019-02-13 14:11:11 +0000

    Added main menu

---
+++ Prototypes/Prototype1/Assets/Scripts/ChangeQuality.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeQuality : MonoBehaviour
+{
+    public Dropdown myDropDown;
+    private int currentQuality = 2;
+
+    public void ChangeLevel()
+    {
+        currentQuality = myDropDown.value;
+        QualitySettings.SetQualityLevel(currentQuality);
+    }
+
+}
---
+++ Prototypes/Prototype1/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeControl : MonoBehaviour
+{
+    public Slider VolumeSlider;
+    public AudioMixer VolumeMixer;
+
+    public void ChangeVolume()
+    {
+        VolumeMixer.SetFloat("MasterVolume", VolumeSlider.value);
+    }
+}
